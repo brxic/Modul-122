e38ee4fbe3eb91862ee1492ffe79961a671d97a0
brxic <152413406+brxic@users.noreply.github.com>
2024-05-03 10:44:47 +0200

    Create script06.sh

---
+++ script06.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Name:         script06.sh
+# Aufruf:       script06.sh <Dateiname>
+# Beschreibung: überprüft Schreibrecht und löscht Datei wenn vorhanden
+# Autor:        Basil Ramseyer
+# Version:      1.0
+# Datum:        03.05.24
