a6066a422dbf144294b98caffb4fff3220df2f9f
brxic <152413406+brxic@users.noreply.github.com>
2024-05-03 10:34:03 +0200

    Update script05.sh

--- script05.sh
+++ script05.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Name:         <Name des Scripts>
-# Aufruf:       <Name des Scripts> [<1. Parameter>] [<2. Parameter>]
-#               [<Beschreibung des 1. Parameters (falls vorhanden)>]
-#               [<Beschreibung des 2. Parameters (falls vorhanden)>]
-# Beschreibung: <Was macht das Script?>
-# Autor:        <Ihr Name>
-# Version:      <Versionsnummer>
-# Datum:        <Änderungsdatum>
+# Name:         script05.sh
+# Aufruf:       ./<scriptname> <gewünschter Unterordnername>
+# Beschreibung: kopiert Dateien von einem File ins andere
+# Autor:        Basil Ramseyer
+# Version:      1.0
+# Datum:        03.05.24
 
 
+cd Testdaten || exit 1 # Wechsel zum Verzeichnis Testdaten; Bei Fehlerabbruch
+
+# Überprüfe, ob das Verzeichnis bereits existiert
+if [ ! -d "$1" ]; then 
+    mkdir "$1" || exit 1 # Verzeichnis erstellen; Bei Fehlerabbruch
+else
+    echo "Das Verzeichnis existiert bereits."
+fi
+
+cp *.* "$1" || exit 1 # Dateien kopieren; Bei Fehlerabbruch
